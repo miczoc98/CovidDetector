ba59b988495a89c552393449ee7f1fdfbe77b9d0
zoczek <michal.m.zoczek@gmail.com>
2021-06-15 23:28:27 +0200

    update join history

--- join_history.py
+++ join_history.py
@@ -1,10 +1,14 @@
+import os
+
 test_params = ["fine_tuning_epochs", "fine_tuning_layers"]
-networks = ["mobileNet", "xception", "vgg19"]
 params = [["10", "20", "50"], ["50", "100", "200"]]
+
+network = "mobileNet"
 
 for i in range(len(test_params)):
     for param in params[i]:
-        for network in networks:
-            with open(f"saved_models/dataset_300/{test_params[i]}/{network}_{param}.h5.history.csv", "a") as f:
-                with open(f"saved_models/dataset_300/{test_params[i]}/{network}_{param}.h5.fine_tuning.history.csv") as f2:
-                    f.writelines(f2)
+        with open(f"saved_models/dataset_300/{test_params[i]}/{network}_{param}.h5.history.csv", "a") as f:
+            with open(f"saved_models/dataset_300/{test_params[i]}/{network}_{param}.h5.fine_tuning.history.csv", "r+") as f2:
+                f.writelines(f2)
+
+            os.remove(f"saved_models/dataset_300/{test_params[i]}/{network}_{param}.h5.fine_tuning.history.csv")
