ba1c15a7e1097b5c24de5f0f17ec864445b1360c
zoczek <michal.m.zoczek@gmail.com>
2021-06-15 16:39:51 +0200

    add basic evaluation script

---
+++ evaluate.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn.metrics import confusion_matrix, classification_report
+import tensorflow.keras as keras
+
+if __name__ == '__main__':
+    model = keras.models.load_model("saved_models/dropouts/xception_0.2.h5")
+
+    generator = keras.preprocessing.image.ImageDataGenerator(1./255)
+
+    validation_gen = generator.flow_from_directory(directory="datasets/dataset_300/test",
+                                                   batch_size=32,
+                                                   target_size=(128, 128),
+                                                   shuffle=False,
+                                                   class_mode="sparse")
+
+    y_pred = model.predict_generator(validation_gen)
+    y_pred = np.argmax(y_pred, axis=1)
+
+    target_names = ["covid", "normal", 'pneumonia']
+
+    print(confusion_matrix(validation_gen.classes, y_pred))
+    print(classification_report(validation_gen.classes, y_pred, target_names=target_names))
